07c0f6518580940c595dc8433c999710da632878
Olga <olgapashkevych@gmail.com>
2018-11-20 13:44:46 +0100

    pig latin add

--- pig-latin/pig-latin.php
+++ pig-latin/pig-latin.php
@@ -9,19 +9,19 @@
 function translate($text) {
     $result = '';
     $words = explode(' ', $text);
-    foreach ($words as $word) {
+    $wordsCount = count($words);
+    foreach ($words as $key => $word) {
         $result .= preg_replace_callback(
-            '/([qu]{2}|[^aeiou]{1,}|[a-z]*)(.*)/',
-        //'/([^qu]{1}|[qu]{2}|[^aeiou]{1,}|[a-z]*)(.*)/',
+            '/((?:xr)|(?:yt)|[^aeiou]*qu|[^aeiou]*)(.+)/',
             function ($matches) {
                 if (empty($matches[0])) return '';
+                if (in_array($matches[1], ['xr', 'yt'])) return $matches[0] . 'ay';
                 return (empty($matches[2])) ? ($matches[1] . 'ay') : ($matches[2] . $matches[1] . 'ay');
             },
             $word
         );
+        if ($wordsCount > 0) $result .= ' ';
     }
 
-    return $result;
+    return trim($result);
 }
-
-print_r(translate('queen'));
