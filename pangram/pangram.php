3b0cc3d8949bf912f31caf61f3a09ace61271517
Olga <olgapashkevych@gmail.com>
2018-12-06 22:07:34 +0100

    refactoring pangram

--- pangram/pangram.php
+++ pangram/pangram.php
@@ -3,5 +3,5 @@
 /** Determine if a sentence is a pangram */
 
 function isPangram (string $string) : bool {
-    return preg_match_all('/([a-z])(?!.*\1)/i', $string) == 26 ? true : false;
+    return preg_match_all('/([a-z])(?!.*\1)/i', $string) == 26;
 }
